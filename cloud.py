698b557796b53835d339b5b18cc7888ac0f8026c
simonlaw101 <simonlaw101@gmail.com>
2021-08-29 03:22:41 +0800

    handle tg callback_data max length 64 bytes

--- cloud.py
+++ cloud.py
@@ -14,53 +14,41 @@ logger = logging.getLogger('FxStock')
 class Cloud:
     def __init__(self, bucket_name):
         self.cmds = {'cloud': self.cloud,
-                     'cloud_download': self.cloud_download,
-                     'cloud_delete': self.cloud_delete,
-                     'cloud_cancel': self.cloud_cancel}
+                     'c1': self.cloud_select,
+                     'c2': self.cloud_download,
+                     'c3': self.cloud_delete,
+                     'c4': self.cloud_cancel}
         self.desc = {'cloud': 'upload/download file'}
         self.examples = {'cloud': Constant.CLOUD_EXAMPLE}
         self.fb = FbService(bucket_name)
 
     def cloud(self, data):
-        args = data['args'].strip()
-        file_url = data.get('file_url', '')
-
-        if data.get('callback_query_id', -1) != -1:
-            data['method'] = ['editMessageText', 'answerCallbackQuery']
-            args_lst = args.split('|')
-            data['text'] = "{}'s Cloud Storage:\n{}".format(args_lst[1],
-                                                            os.path.split(args_lst[0])[1])
-            btn_lst = [[{'text': 'Download', "callback_data": "/cloud_download " + args},
-                        {'text': 'Delete', "callback_data": "/cloud_delete " + args}],
-                       [{'text': 'Cancel', "callback_data": "/cloud_cancel " + args}]]
-            data['reply_markup'] = {"inline_keyboard": btn_lst}
-            return
-
         data['method'] = 'sendMessage'
+        file_url = data.get('file_url', '')
         if file_url != '':
-            if args != '' and not re.match('^[a-zA-Z0-9_-]+$', args):
-                data['text'] = '"{}" is not a valid file name!'.format(args)
+            args = data['args'].strip()
+            if (args != '' and not re.match('^[a-zA-Z0-9_-]+$', args)) or len(args) > 20:
+                data['text'] = 'Filename must be alphanumeric and within 20 characters!'.format(args)
             else:
                 data['text'] = self.upload_file(args, file_url, data['from_id'])
         else:
             self.list_file(data)
 
     def list_file(self, data):
-        sender_name = data['sender_name']
         files = self.fb.list_file(str(data['from_id']))
         if len(files) == 0:
-            data['text'] = "{}'s Cloud Storage is empty".format(sender_name)
+            data['text'] = 'Cloud Storage is empty'
         else:
-            data['text'] = "{}'s Cloud Storage:".format(data['sender_name'])
-            btn_lst = [[{'text': file['name'], "callback_data": "/cloud {}|{}".format(file['full_path'], sender_name)}]
+            data['text'] = 'Cloud Storage:'
+            btn_lst = [[{'text': file['name'], 'callback_data': '/c1 {}'.format(file['full_path'])}]
                        for file in files]
-            data['reply_markup'] = {"inline_keyboard": btn_lst}
+            data['reply_markup'] = {'inline_keyboard': btn_lst}
 
     def upload_file(self, args, file_url, from_id):
         full_path = ''
         filename = self.get_des_filename(args, file_url)
         try:
-            with tempfile.NamedTemporaryFile(mode="wb", delete=False) as f:
+            with tempfile.NamedTemporaryFile(mode='wb', delete=False) as f:
                 full_path = f.name
                 resp = HttpService.get(file_url)
                 f.write(resp.content)
@@ -78,38 +66,39 @@ class Cloud:
             return datetime.now().strftime('%Y%m%d%H%M%S%f')[:-3] + splitext(file_url)[1]
         return args + splitext(file_url)[1]
 
-    def cloud_cancel(self, data):
-        args = data['args'].strip()
-        args_lst = args.split('|')
+    def cloud_select(self, data):
         if data.get('callback_query_id', -1) != -1:
             data['method'] = ['editMessageText', 'answerCallbackQuery']
-            data['sender_name'] = args_lst[1]
-            data['from_id'] = os.path.split(args_lst[0])[0]
+            args = data['args'].strip()
+            data['text'] = 'Cloud Storage:\n{}'.format(os.path.split(args)[1])
+            btn_lst = [[{'text': 'Download', 'callback_data': '/c2 ' + args},
+                        {'text': 'Delete', 'callback_data': '/c3 ' + args}],
+                       [{'text': 'Cancel', 'callback_data': '/c4 ' + args}]]
+            data['reply_markup'] = {'inline_keyboard': btn_lst}
+            return
+
+    def cloud_cancel(self, data):
+        if data.get('callback_query_id', -1) != -1:
+            args = data['args'].strip()
+            data['method'] = ['editMessageText', 'answerCallbackQuery']
+            data['from_id'] = os.path.split(args)[0]
             self.list_file(data)
 
     def cloud_download(self, data):
-        args = data['args'].strip()
-        args_lst = args.split('|')
         if data.get('callback_query_id', -1) != -1:
+            args = data['args'].strip()
             if os.path.splitext(args)[1].lower() in ['.gif', '.jpg', '.jpeg', '.png']:
                 data['method'] = ['sendPhoto', 'answerCallbackQuery', 'editMessageText']
-                data['text'] = ("{}'s Cloud Storage:\n"
-                                "{} downloaded successfully").format(args_lst[1],
-                                                                     os.path.split(args_lst[0])[1])
-                data['photo'] = self.fb.get_file_url(args_lst[0])
+                data['text'] = '{} downloaded successfully'.format(os.path.split(args)[1])
+                data['photo'] = self.fb.get_file_url(args)
             else:
                 data['method'] = ['sendDocument', 'answerCallbackQuery', 'editMessageText']
-                data['text'] = ("{}'s Cloud Storage:\n"
-                                "{} downloaded successfully").format(args_lst[1],
-                                                                     os.path.split(args_lst[0])[1])
-                data['document'] = self.fb.get_file_url(args_lst[0])
+                data['text'] = '{} downloaded successfully'.format(os.path.split(args)[1])
+                data['document'] = self.fb.get_file_url(args)
 
     def cloud_delete(self, data):
-        args = data['args']
-        args_lst = args.split('|')
         if data.get('callback_query_id', -1) != -1:
-            self.fb.delete_file(args_lst[0])
+            args = data['args']
+            self.fb.delete_file(args)
             data['method'] = ['editMessageText', 'answerCallbackQuery']
-            data['text'] = ("{}'s Cloud Storage:\n"
-                            "{} deleted successfully").format(args_lst[1],
-                                                              os.path.split(args_lst[0])[1])
+            data['text'] = '<s>{}</s> deleted successfully'.format(os.path.split(args)[1])
